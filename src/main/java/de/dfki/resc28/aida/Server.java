6dbc50eeef7b299caf87aaf1dc795e2949491414
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2016-08-02 18:55:48 +0200

    Improved application configuration.
    Configuration is performed only once.
    Use hostName or baseURI property to configure base URI.

--- src/main/java/de/dfki/resc28/aida/Server.java
+++ src/main/java/de/dfki/resc28/aida/Server.java
@@ -29,21 +29,23 @@ import de.dfki.resc28.igraphstore.jena.TDBGraphStore;
  *
  */
 @ApplicationPath("/")
-public class Server extends Application 
+public class Server extends Application
 {
-	public static IGraphStore fGraphStore;
-	
+	public static IGraphStore fGraphStore = null;
+
 	@Override
-    public Set<Object> getSingletons() 
+    public Set<Object> getSingletons()
     {
 		configure();
-		initGraphStore();
 		DTrackActionProvider bla = new DTrackActionProvider(fGraphStore);
 		return new HashSet<Object>(Arrays.asList(bla));
     }
-	
-	public void configure()
+
+	public static synchronized void configure()
 	{
+            if (fGraphStore != null)
+                return;
+
 		try
 		{
 
@@ -63,7 +65,14 @@ public class Server extends Application
 			p.load(is);
 
 			String storage = p.getProperty("graphStore");
-			
+                        String baseURI = p.getProperty("baseURI");
+                        if (baseURI == null) {
+                            System.out.println("AIDA: baseURI property is null, use hostName property");
+                            String hostName = p.getProperty("hostName", "localhost");
+                            baseURI = "http://" + hostName;
+                        }
+                        System.out.format("AIDA: baseURI = %s%n", baseURI);
+
 			if (storage.equals("fuseki"))
 			{
                                 String dataEndpoint = p.getProperty("dataEndpoint");
@@ -81,40 +90,43 @@ public class Server extends Application
 				else
 					Server.fGraphStore = new TDBGraphStore();
 			}
+                        initGraphStore(baseURI);
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 	}
-	
-	public void initGraphStore()
+
+	public static synchronized void initGraphStore(String baseURI)
 	{
+		System.out.println("AIDA: Initializing Graph Storage ...");
 		Server.fGraphStore.clearDefaultGraph();
-		
+
 		Model machineModel = ModelFactory.createDefaultModel();
-		RDFDataMgr.read(machineModel, getClass().getClassLoader().getResourceAsStream("model.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/model", machineModel);
-		
+		RDFDataMgr.read(machineModel, Server.class.getClassLoader().getResourceAsStream("model.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/model", machineModel);
+
 		Model initialState = ModelFactory.createDefaultModel();
-		RDFDataMgr.read(initialState, getClass().getClassLoader().getResourceAsStream("init.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/model/initial", initialState);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api", initialState);
-		
-		Model configureAction = ModelFactory.createDefaultModel(); 
-		RDFDataMgr.read(configureAction, getClass().getClassLoader().getResourceAsStream("configure.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/actions/configure", configureAction);
-		
-		Model startMeasurementAction = ModelFactory.createDefaultModel(); 
-		RDFDataMgr.read(startMeasurementAction, getClass().getClassLoader().getResourceAsStream("startMeasurement.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/actions/startMeasurement", startMeasurementAction);
-		
-		Model stopMeasurementAction = ModelFactory.createDefaultModel(); 
-		RDFDataMgr.read(stopMeasurementAction, getClass().getClassLoader().getResourceAsStream("stopMeasurement.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/actions/stopMeasurement", stopMeasurementAction);
-		
-		Model tearDownAction = ModelFactory.createDefaultModel(); 
-		RDFDataMgr.read(tearDownAction, getClass().getClassLoader().getResourceAsStream("tearDown.ttl"), Lang.TURTLE);
-		Server.fGraphStore.replaceNamedGraph("http://localhost:8080/api/actions/tearDown", tearDownAction);
+		RDFDataMgr.read(initialState, Server.class.getClassLoader().getResourceAsStream("init.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/model/initial", initialState);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api", initialState);
+
+		Model configureAction = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(configureAction, Server.class.getClassLoader().getResourceAsStream("configure.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/actions/configure", configureAction);
+
+		Model startMeasurementAction = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(startMeasurementAction, Server.class.getClassLoader().getResourceAsStream("startMeasurement.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/actions/startMeasurement", startMeasurementAction);
+
+		Model stopMeasurementAction = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(stopMeasurementAction, Server.class.getClassLoader().getResourceAsStream("stopMeasurement.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/actions/stopMeasurement", stopMeasurementAction);
+
+		Model tearDownAction = ModelFactory.createDefaultModel();
+		RDFDataMgr.read(tearDownAction, Server.class.getClassLoader().getResourceAsStream("tearDown.ttl"), Lang.TURTLE);
+		Server.fGraphStore.replaceNamedGraph(baseURI+"/api/actions/tearDown", tearDownAction);
+                System.out.println("AIDA: Graph Storage Initialized");
 	}
 }
